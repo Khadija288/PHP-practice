11682487e1eb8b8ef1678bdd2dc69766d4462ccd
Khadija288 <khadijarani289@gmail.com>
2025-08-31 20:20:58 +0500

    edit

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 // String variable
 $name = "Khadija"
 // Integer variable
-$age = 17;
+// $age = 17;
 // Float variable
-$height = 5.4;
+// $height = 5.4;
 // boolean variables
-$isstudent = true
-echo $name;
-echo $age;
-echo $height;
-define("PI", 3.1416);
+// $isstudent = true
+// echo $name;
+// echo $age;
+// echo $height;
+// define("PI", 3.1416);
 const SITE_NAME = "MyWebsite";
 $a = 4;
 $b = 5;
