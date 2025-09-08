0d6d5d80a00d300270a7a95f1d74e532a8bb5920
Khadija288 <khadijarani289@gmail.com>
2025-09-08 23:25:28 +0500

    Edit

--- FormHandling.php
+++ FormHandling.php
@@ -9,12 +9,13 @@
 <body>
     <form class="container mt-5 m-5 p-5 mx-5">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label class="form-label">Email address</label>
+    <input type="email" class="form-control" >
   </div>
+  
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label class="form-label">Password</label>
+    <input type="password" class="form-control" >
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
