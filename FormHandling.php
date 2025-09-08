1a1bca2178f993428466b53851594322561f8af9
Khadija288 <khadijarani289@gmail.com>
2025-09-08 23:25:43 +0500

    edit

--- FormHandling.php
+++ FormHandling.php
@@ -12,7 +12,10 @@
     <label class="form-label">Email address</label>
     <input type="email" class="form-control" >
   </div>
-  
+  <div class="mb-3">
+    <label class="form-label">Email address</label>
+    <input type="email" class="form-control" >
+  </div>
   <div class="mb-3">
     <label class="form-label">Password</label>
     <input type="password" class="form-control" >
