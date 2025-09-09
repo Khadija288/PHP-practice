8503385238d2390276da7cd3ad0ce3ef8317c020
Khadija288 <khadijarani289@gmail.com>
2025-09-09 20:22:28 +0500

    edit

--- FormHandling.php
+++ FormHandling.php
@@ -26,6 +26,7 @@
     <label class="form-label">Password</label>
     <input type="password" class="form-control"  placeholder="Enter Passward">
   </div>
+  
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </body>
