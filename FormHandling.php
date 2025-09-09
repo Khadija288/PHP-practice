ecb7b7542b172a391691f9c2f54dd1e1be6835fe
Khadija288 <khadijarani289@gmail.com>
2025-09-09 20:25:03 +0500

    edit

--- FormHandling.php
+++ FormHandling.php
@@ -28,7 +28,9 @@
    <div class="valid-feedback">
       Looks good!
     </div>
-    
+    <div class="invalid-feedback">
+      Enter valid data!
+    </div>
   <div class="mb-3">
     <label class="form-label">Password</label>
     <input type="password" class="form-control"  placeholder="Enter Passward">
