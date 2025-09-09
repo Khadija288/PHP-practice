d90467fe2c5252487d4cc39dc8f6b8ccad741810
Khadija288 <khadijarani289@gmail.com>
2025-09-09 20:20:06 +0500

    edit

--- FormHandling.php
+++ FormHandling.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <form class="container mt-5 m-5 p-5 mx-5 needs-validation">
+    <form class="container mt-5 m-5 p-5 mx-5 needs-validation" novalidate>
   <div class="mb-3">
     <label class="form-label">Name</label>
     <input type="text" placeholder="Enter name" class="form-control">
